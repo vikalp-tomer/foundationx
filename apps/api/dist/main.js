17f54945b6d0688e29f09d4d92e613ea705f0dab
vikalp-tomer <vikalptomar15@gmail.com>
2024-07-10 09:05:41 +0530

    add nx

--- apps/api/dist/main.js
+++ apps/api/dist/main.js
@@ -2,8 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const sample_lib_1 = require("@foundation/sample-lib");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    console.log('add', (0, sample_lib_1.add)(1, 2));
     await app.listen(3000);
 }
 bootstrap();
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
+import { add } from "@foundation/sample-lib";
 
 export default function Home() {
-  return <main>HI there</main>;
+  return <main>HI {add(1, 2)}</main>;
 }
